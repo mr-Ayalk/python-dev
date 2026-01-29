d35a2fc31e853d15b5913b657b91a8364b182168
mr-Ayalk <ayalkteketel7@gmail.com>
2026-01-29 11:46:12 +0300

    exercise 3 done

---
+++ day-1/Exercise-Folder/Exercise3.py
@@ -0,0 +1,14 @@
+#Madlibs game
+#word game where you create a story by filling in the blanks with different types of words like nouns, verbs, adjectives, etc.
+
+adjective1 = input("Enter an adjective(description): ")
+noun1=input("Enter a noun(person, place, or thing): ")
+adjective2=input("Enter another adjective(description): ")
+verb1=input("Enter a verb ending with ing(action word): ")
+
+adjective3=input("Enter one more adjective(description): ")
+
+print(f"Today I went to a {adjective1} zoom")
+print(f"I saw a {noun1} that was very {adjective2}")
+print(f"It was {verb1} in a very {adjective3} way!")
+    
