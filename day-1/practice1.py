bb329669b010d68de6e53619a147ebb6247aaa63
mr-Ayalk <ayalkteketel7@gmail.com>
2026-01-29 10:40:02 +0300

    practice one done

---
+++ day-1/practice1.py
@@ -0,0 +1,7 @@
+#This is my first Python program-we use # for comments and we can use double or single quotes for strings-for multiple lines we can use triple quotes
+
+'''print("Day one,Let's Gooooooooo!")
+print('No reason to stop')'''
+
+print("""Day one,Let's Gooooooooo!
+No reason to stop""")
