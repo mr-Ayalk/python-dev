efd9746083ec928dc7f30fdab4563f5b24cba6af
mr-Ayalk <ayalkteketel7@gmail.com>
2026-01-30 13:06:56 +0300

    day2-practice 1 done

---
+++ day2/Practice-Folder/practice1.py
@@ -0,0 +1,19 @@
+#Topic :Conditional Statements
+
+#a one line shortcut for if-else statement(ternary operator) print or assign one of two values based on a condition X if condition else Y
+
+# Example 1: Using ternary operator to print a value based on a condition
+age = 20
+status = "Adult" if age >= 18 else "Minor"
+print(status)  # Output: Adult
+
+
+num=5
+a=6
+b=7
+age=12
+temprature=34
+
+print("You can vote") if age>=18 else print("You cannot vote")
+
+print("Positive" if num>0 else "Negative or Zero")
